8ee40ce0de1f628a3e9f9165b006f8cf6c2ccf11
HalushkaYura <yuragalyshka@gmail.com>
2023-11-29 12:45:24 +0200

    Loaded project

---
+++ MintStores/Data/DataService.cs
@@ -0,0 +1,29 @@
+﻿namespace MintStores.Data
+{
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Data.SqlClient;
+    using System.Threading.Tasks;
+    using Dapper;
+    using MintStores.Models;
+
+    public class DataService
+    {
+        private readonly string _connectionString;
+
+        public DataService(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task<IEnumerable<Category>> GetData()
+        {
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    await connection.OpenAsync();
+                    return await connection.QueryAsync<Category>("SELECT * FROM Categories");
+                }
+        }
+
+    }
+}
